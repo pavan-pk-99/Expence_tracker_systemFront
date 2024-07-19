d2a2a8bcdf8d2e755f60518c011a8417afe9f8ba
pavan-pk-99 <76556706+pavan-pk-99@users.noreply.github.com>
2024-07-19 23:23:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 
 reportWebVitals();
