409d6bd72226e9d42e57fa5df2658b40075b46fc
pavan-pk-99 <76556706+pavan-pk-99@users.noreply.github.com>
2024-07-19 23:43:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
